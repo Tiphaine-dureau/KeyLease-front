f184930511a88c6f818c54428ec090370717971c
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-01-16 11:33:12 +0100

    Configured apiUrl environment for prod or dev and called helloWorld from back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'key-lease-front';
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  getHelloWorld(){
+    return this.http.get(`${environment.apiUrl}/helloworld`)
+  }
+
+  ngOnInit(): void {
+    this.getHelloWorld().subscribe((data)=> console.log(data))
+  }
 }
