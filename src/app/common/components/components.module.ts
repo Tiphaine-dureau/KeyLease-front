ec921c2c98ab0a71427c6c491a1ddfa6b1bb23f4
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-01-30 10:58:41 +0100

    Added toolbar component with temporary routes

--- src/app/common/components/components.module.ts
+++ src/app/common/components/components.module.ts
@@ -2,18 +2,31 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SpinnerComponent} from "./spinner/spinner.component";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
   declarations: [
-    SpinnerComponent
+    SpinnerComponent,
+    ToolbarComponent
   ],
   exports: [
-    SpinnerComponent
+    SpinnerComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterLink,
   ]
 })
 export class ComponentsModule {
