87455dc21aad604f218fc884108b160ac0303a5e
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2023-03-28 20:03:52 +0200

    Added POST/ fixtures-inventory

--- src/app/features/fixture-inventory/services/fixture-inventory.service.ts
+++ src/app/features/fixture-inventory/services/fixture-inventory.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {FixtureInventoryBusinessModel} from "../../../common/business-models/fixture-inventory.business-model";
 import {environment} from "../../../../environments/environment";
+import {PostFixtureInventoryModel} from "./post-fixture-inventory.model";
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class FixtureInventoryService {
 
   public getFixtureInventory(fixtureInventoryId: string): Observable<FixtureInventoryBusinessModel> {
     return this.http.get<FixtureInventoryBusinessModel>(`${environment.apiUrl}/fixtures-inventory/${fixtureInventoryId}`);
+  }
+
+  public addFixtureInventory(model: PostFixtureInventoryModel): Observable<FixtureInventoryBusinessModel> {
+    return this.http.post<FixtureInventoryBusinessModel>(`${environment.apiUrl}/fixtures-inventory`, model);
   }
 }
